3859776f51b42b2a4129cdd21df62d18dc833324
Akkadate <89246175+Akkadate@users.noreply.github.com>
2026-06-24 14:38:09 +0700

    'sss

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -9,7 +9,7 @@ import './globals.css';
 
 const prompt = Prompt({
   subsets: ['thai', 'latin'],
-  weight: ['300', '400', '500', '600', '700'],
+  weight: ['400', '500', '600', '700'],
   display: 'swap',
 });
 
