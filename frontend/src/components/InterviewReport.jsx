1cfc1b64260dcd27b63f6cef3920ee012b0e87e6
Akkadate <89246175+Akkadate@users.noreply.github.com>
2026-06-24 15:04:40 +0700

    fix: widen program column, narrow numeric columns in report table

--- frontend/src/components/InterviewReport.jsx
+++ frontend/src/components/InterviewReport.jsx
@@ -67,10 +67,10 @@ export default function InterviewReport({ faculty = null, academicYear = null, d
         <table className="w-full table-fixed bg-white text-sm">
           <colgroup>
             <col style={{ width: '22%' }} />
-            <col style={{ width: '30%' }} />
-            <col style={{ width: '9%' }} />
-            <col style={{ width: '9%' }} />
-            <col style={{ width: '10%' }} />
+            <col style={{ width: '40%' }} />
+            <col style={{ width: '6%' }} />
+            <col style={{ width: '6%' }} />
+            <col style={{ width: '6%' }} />
             <col style={{ width: '20%' }} />
           </colgroup>
           <thead className="bg-gray-50">
